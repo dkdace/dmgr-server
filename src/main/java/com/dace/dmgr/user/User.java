c8796a7706526d7f703989e20e3f1d5b0fa02b85
dkdace <darkdace2@naver.com>
2022-09-09 02:54:10 +0900

    레벨,랭크 관련 메소드 추가 / 로비 상태창 추가

--- src/main/java/com/dace/dmgr/user/User.java
+++ src/main/java/com/dace/dmgr/user/User.java
@@ -3,6 +3,7 @@ package com.dace.dmgr.user;
 import com.dace.dmgr.system.SkinManager;
 import com.dace.dmgr.util.HasCooldown;
 import com.dace.dmgr.util.YamlModel;
+import fr.minuskube.netherboard.bukkit.BPlayerBoard;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
@@ -11,15 +12,16 @@ import static com.dace.dmgr.system.EntityList.combatUserList;
 import static com.dace.dmgr.system.EntityList.userList;
 
 public class User extends YamlModel implements HasCooldown {
+    public final BPlayerBoard lobbySidebar;
     private final Player player;
     private final UserConfig userConfig;
     public boolean resourcePack = false;
     public PlayerResourcePackStatusEvent.Status resourcePackStatus = null;
     private String name;
     private int xp = 0;
-    private int level = 0;
+    private int level = 1;
     private int money = 0;
-    private int rank = 0;
+    private int rank = 100;
 
     public User(Player player) {
         super("User", player.getUniqueId().toString());
@@ -30,6 +32,7 @@ public class User extends YamlModel implements HasCooldown {
         this.level = loadValue("level", level);
         this.money = loadValue("money", money);
         this.rank = loadValue("rank", rank);
+        this.lobbySidebar = new BPlayerBoard(player, "lobbySidebar");
         userList.put(player.getUniqueId(), this);
         saveConfig();
     }
@@ -108,6 +111,72 @@ public class User extends YamlModel implements HasCooldown {
         player.getActivePotionEffects().forEach((potionEffect ->
                 player.removePotionEffect(potionEffect.getType())));
         combatUserList.remove(player.getUniqueId());
+    }
+
+    public String getLevelPrefix() {
+        String color = "";
+
+        if (level <= 100)
+            color = "§f§l";
+        else if (level <= 200)
+            color = "§a§l";
+        else if (level <= 300)
+            color = "§b§l";
+        else if (level <= 400)
+            color = "§d§l";
+        else
+            color = "§e§l";
+
+        return color + "[ Lv." + level + " ]";
+    }
+
+    public String getTierPrefix() {
+        if (rank <= 0)
+            return "§8§l[ F ]";
+        else if (rank <= 300)
+            return "§7§l[ E ]";
+        else if (rank <= 600)
+            return "§f§l[ D ]";
+        else if (rank <= 1000)
+            return "§a§l[ C ]";
+        else if (rank <= 1500)
+            return "§b§l[ B ]";
+        else if (rank <= 2000)
+            return "§d§l[ A ]";
+        else
+            return "§e§l[ S ]";
+    }
+
+    public int getNextLevelXp() {
+        return 250 + (level * 50);
+    }
+
+    public int getNextTierScore() {
+        if (rank <= 0)
+            return 0;
+        else if (rank <= 300)
+            return 300;
+        else if (rank <= 600)
+            return 600;
+        else if (rank <= 1000)
+            return 1000;
+        else if (rank <= 1500)
+            return 1500;
+        else
+            return 2000;
+    }
+
+    public int getCurrentTierScore() {
+        if (rank <= 300)
+            return 0;
+        else if (rank <= 600)
+            return 300;
+        else if (rank <= 1000)
+            return 600;
+        else if (rank <= 1500)
+            return 1000;
+        else
+            return 1500;
     }
 
     @Override
