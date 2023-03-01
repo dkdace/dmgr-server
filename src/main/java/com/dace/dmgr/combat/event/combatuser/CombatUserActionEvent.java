91ee8af027e4a1baa7ca89f35f46d6d762904818
Bonwoong <thesmartist@naver.com>
2023-03-02 03:26:32 +0900

    feat: lombokfied CombatUserActionEvent.java

--- src/main/java/com/dace/dmgr/combat/event/combatuser/CombatUserActionEvent.java
+++ src/main/java/com/dace/dmgr/combat/event/combatuser/CombatUserActionEvent.java
@@ -3,12 +3,15 @@ package com.dace.dmgr.combat.event.combatuser;
 import com.dace.dmgr.combat.action.ActionKey;
 import com.dace.dmgr.combat.entity.CombatUser;
 import com.dace.dmgr.combat.event.CombatUserEvent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * 플레이어가 상호작용할 시 호출되는 이벤트.
  *
  * <p>상호작용은 좌클릭, 우클릭 등을 말하며 종류는 {@link ActionKey}에 있다.</p>
  */
+@Getter
 public class CombatUserActionEvent extends CombatUserEvent {
     /** 상호작용 종류 */
     private final ActionKey actionKey;
@@ -22,9 +25,5 @@ public class CombatUserActionEvent extends CombatUserEvent {
     public CombatUserActionEvent(CombatUser combatUser, ActionKey actionKey) {
         super(combatUser);
         this.actionKey = actionKey;
-    }
-
-    public ActionKey getActionKey() {
-        return actionKey;
     }
 }
