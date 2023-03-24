7eacaa7b416c3f6ecc99ff87177a55b2ad3bbfeb
dkdace <darkdace2@naver.com>
2023-03-24 23:40:33 +0900

    ItemBuilder Lombok 적용

--- src/main/java/com/dace/dmgr/gui/ItemBuilder.java
+++ src/main/java/com/dace/dmgr/gui/ItemBuilder.java
@@ -3,6 +3,7 @@ package com.dace.dmgr.gui;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import com.shampaggon.crackshot.CSUtility;
+import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemFlag;
@@ -19,12 +20,13 @@ import java.util.UUID;
  */
 public class ItemBuilder {
     /** 크랙샷 아이템을 생성하기 위한 크랙샷 객체 */
-    public static final CSUtility csUtility = new CSUtility();
+    private static final CSUtility csUtility = new CSUtility();
     /** 플레이어 머리 생성에 사용하는 필드 객체 */
     private static Field profileField;
     /** 생성할 아이템 객체 */
     private final ItemStack itemStack;
     /** 생성할 아이템의 정보 객체 */
+    @Getter
     private final ItemMeta itemMeta;
 
     /**
@@ -99,10 +101,6 @@ public class ItemBuilder {
         ItemBuilder itemBuilder = new ItemBuilder(weapon).addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE);
         itemBuilder.getItemMeta().setUnbreakable(true);
         return itemBuilder;
-    }
-
-    public ItemMeta getItemMeta() {
-        return itemMeta;
     }
 
     /**
