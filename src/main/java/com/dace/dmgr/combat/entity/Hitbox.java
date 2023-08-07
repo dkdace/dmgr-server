0287c5b09ac7becdcfc9cfb57b4feacaccc7f040
dkdace <darkdace2@naver.com>
2023-08-07 20:04:54 +0900

    Hitbox 리팩토링

--- src/main/java/com/dace/dmgr/combat/entity/Hitbox.java
+++ src/main/java/com/dace/dmgr/combat/entity/Hitbox.java
@@ -2,11 +2,13 @@ package com.dace.dmgr.combat.entity;
 
 import com.dace.dmgr.util.LocationUtil;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.Location;
 
 /**
  * 공격 판정에 사용하는 히트박스 클래스.
  */
+@RequiredArgsConstructor
 public class Hitbox {
     /** 가로. 단위: 블록 */
     @Getter
@@ -26,26 +28,7 @@ public class Hitbox {
     /** 중앙 위치 */
     private Location center;
 
-    /**
-     * 히트박스 인스턴스를 생성한다.
-     *
-     * @param offsetX 위치 오프셋. 왼쪽(-) / 오른쪽(+)
-     * @param offsetY 위치 오프셋. 아래(-) / 위(+)
-     * @param offsetZ 위치 오프셋. 뒤(-) / 앞(+)
-     * @param sizeX   가로
-     * @param sizeY   높이
-     * @param sizeZ   세로
-     */
-    public Hitbox(double offsetX, double offsetY, double offsetZ, double sizeX, double sizeY, double sizeZ) {
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
-        this.offsetZ = offsetZ;
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
-        this.sizeZ = sizeZ;
-    }
-
-    public Location getCenter() {
+    public final Location getCenter() {
         return center.clone();
     }
 
@@ -54,7 +37,7 @@ public class Hitbox {
      *
      * @param location 중앙 위치
      */
-    public void setCenter(Location location) {
+    public final void setCenter(Location location) {
         this.center = location.clone();
         center.setPitch(0);
         center = LocationUtil.getLocationFromOffset(center, offsetX, offsetY, offsetZ);
