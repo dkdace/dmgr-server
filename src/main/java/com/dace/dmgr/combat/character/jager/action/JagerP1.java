f570a44d63dba1c9e6d145776216d98a6920dde6
dkdace <darkdace2@naver.com>
2024-03-29 00:14:16 +0900

    '예거' 패시브 1번 - 발동 조건 수정

--- src/main/java/com/dace/dmgr/combat/character/jager/action/JagerP1.java
+++ src/main/java/com/dace/dmgr/combat/character/jager/action/JagerP1.java
@@ -1,8 +1,10 @@
 package com.dace.dmgr.combat.character.jager.action;
 
+import com.dace.dmgr.combat.CombatUtil;
 import com.dace.dmgr.combat.action.ActionKey;
 import com.dace.dmgr.combat.action.skill.AbstractSkill;
 import com.dace.dmgr.combat.entity.CombatUser;
+import com.dace.dmgr.combat.entity.Damageable;
 import com.dace.dmgr.util.task.IntervalTask;
 import com.dace.dmgr.util.task.TaskUtil;
 import lombok.NonNull;
@@ -44,7 +46,10 @@ public final class JagerP1 extends AbstractSkill {
         if (!skill1.isDurationFinished() && skill1.entity != null) {
             JagerA1Entity jagerA1Entity = skill1.entity;
 
-            return jagerA1Entity.getHitboxes()[0].isInHitbox(combatUser.getEntity().getLocation(), JagerP1Info.DETECT_RADIUS);
+            Damageable target = (Damageable) CombatUtil.getNearCombatEntity(combatUser.getGame(), combatUser.getEntity().getLocation(),
+                    JagerP1Info.DETECT_RADIUS, combatEntity -> combatEntity == jagerA1Entity);
+
+            return target != null;
         }
 
         return false;
@@ -55,7 +60,7 @@ public final class JagerP1 extends AbstractSkill {
         setDuration();
         combatUser.getMoveModule().getSpeedStatus().addModifier("JagerP1", JagerP1Info.SPEED);
 
-        TaskUtil.addTask(this, new IntervalTask(i -> canActivate(), isCancelled -> {
+        TaskUtil.addTask(taskRunner, new IntervalTask(i -> canActivate(), isCancelled -> {
             setDuration(0);
             combatUser.getMoveModule().getSpeedStatus().removeModifier("JagerP1");
         }, 1));
