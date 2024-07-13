d41d86fda451eec073b4fc5575a1f9ed59e5a54e
dkdace <darkdace2@naver.com>
2024-07-14 00:16:42 +0900

    인게임 전체/팀 채팅 기능 추가

--- src/main/java/com/dace/dmgr/event/listener/OnAsyncPlayerChat.java
+++ src/main/java/com/dace/dmgr/event/listener/OnAsyncPlayerChat.java
@@ -1,6 +1,9 @@
 package com.dace.dmgr.event.listener;
 
 import com.dace.dmgr.GeneralConfig;
+import com.dace.dmgr.combat.entity.CombatUser;
+import com.dace.dmgr.game.Game;
+import com.dace.dmgr.game.GameUser;
 import com.dace.dmgr.user.User;
 import com.dace.dmgr.user.UserData;
 import com.dace.dmgr.util.CooldownUtil;
@@ -12,6 +15,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 
 public final class OnAsyncPlayerChat implements Listener {
     /** 쿨타임 ID */
@@ -34,25 +38,46 @@ public final class OnAsyncPlayerChat implements Listener {
         }
 
         Bukkit.getServer().getConsoleSender().sendMessage(MessageFormat.format("<{0}> {1}", userData.getDisplayName(), event.getMessage()));
+
         if (user.getMessageTarget() == null) {
-            Bukkit.getOnlinePlayers().forEach((Player player2) -> {
-                UserData userData2 = UserData.fromPlayer(player2);
-                if (!userData2.isBlockedPlayer(userData)) {
-                    player2.sendMessage(MessageFormat.format("<{0}> {1}", userData.getDisplayName(), event.getMessage()));
-                    SoundUtil.play(userData2.getConfig().getChatSound().getSound(), player2, 1000, Math.sqrt(2));
-                }
-            });
-        } else {
-            User targetUser = user.getMessageTarget();
-            UserData targetUserData = targetUser.getUserData();
+            GameUser gameUser = GameUser.fromUser(user);
 
-            player.sendMessage(MessageFormat.format("<{0}> §7{1}", userData.getDisplayName(), event.getMessage()));
-            SoundUtil.play(userData.getConfig().getChatSound().getSound(), player, 1000, Math.sqrt(2));
+            if (gameUser == null) {
+                Bukkit.getOnlinePlayers().forEach((Player player2) ->
+                        sendMessage(user, User.fromPlayer(player2), MessageFormat.format("<{0}> {1}", userData.getDisplayName(), event.getMessage())));
+            } else if (gameUser.getGame().getPhase() != Game.Phase.READY && gameUser.getGame().getPhase() != Game.Phase.PLAYING) {
+                CombatUser combatUser = CombatUser.fromUser(user);
 
-            if (!targetUserData.isBlockedPlayer(userData)) {
-                targetUser.getPlayer().sendMessage(MessageFormat.format("<{0} §7님의 개인 메시지§f> §7{1}", userData.getDisplayName(), event.getMessage()));
-                SoundUtil.play(targetUserData.getConfig().getChatSound().getSound(), targetUser.getPlayer(), 1000, Math.sqrt(2));
+                ArrayList<GameUser> targets;
+                if (gameUser.isTeamChat())
+                    targets = gameUser.getGame().getTeamUserMap().get(gameUser.getTeam());
+                else
+                    targets = gameUser.getGame().getGameUsers();
+
+                targets.forEach(gameUser2 -> sendMessage(user, gameUser2.getUser(), MessageFormat.format("§7§l[{0}] §f<{1}§l[{2}]§f{3}> {4}",
+                        gameUser.isTeamChat() ? "팀" : "전체", gameUser.getTeam().getColor(),
+                        combatUser == null ? "미선택" : combatUser.getCharacterType().getCharacter().getName(), player.getName(), event.getMessage())));
             }
+        } else {
+            sendMessage(user, user, MessageFormat.format("<{0}> §7{1}", userData.getDisplayName(), event.getMessage()));
+            sendMessage(user, user.getMessageTarget(), MessageFormat.format("<{0} §7님의 개인 메시지§f> §7{1}", userData.getDisplayName(), event.getMessage()));
         }
+    }
+
+    /**
+     * 대상 플레이어에게 메시지를 전송하고 효과음을 재생한다.
+     *
+     * @param sender   발신 플레이어
+     * @param receiver 수신 플레이어
+     * @param message  메시지
+     */
+    private static void sendMessage(User sender, User receiver, String message) {
+        UserData receiverUserData = receiver.getUserData();
+
+        if (receiverUserData.isBlockedPlayer(sender.getUserData()))
+            return;
+
+        receiver.getPlayer().sendMessage(message);
+        SoundUtil.play(receiverUserData.getConfig().getChatSound().getSound(), receiver.getPlayer(), 1000, Math.sqrt(2));
     }
 }
