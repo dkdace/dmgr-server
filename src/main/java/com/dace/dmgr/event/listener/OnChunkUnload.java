e586a70d136d78b1da69aab107f993f88019174a
dkdace <darkdace2@naver.com>
2024-08-23 00:00:28 +0900

    청크 언로딩 방지가 제대로 작동하지 않는 문제 수정 (이벤트 우선도 증가)

--- src/main/java/com/dace/dmgr/event/listener/OnChunkUnload.java
+++ src/main/java/com/dace/dmgr/event/listener/OnChunkUnload.java
@@ -1,11 +1,12 @@
 package com.dace.dmgr.event.listener;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
 public final class OnChunkUnload implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public static void event(ChunkUnloadEvent event) {
         event.setCancelled(true);
     }
