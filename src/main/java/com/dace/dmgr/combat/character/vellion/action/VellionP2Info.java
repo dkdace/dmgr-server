1bc977c30a9c761258d11ad9c47a7b2e52ced8f2
dkdace <darkdace2@naver.com>
2024-09-11 00:44:28 +0900

    '벨리온' - 동작 설명 개편

--- src/main/java/com/dace/dmgr/combat/character/vellion/action/VellionP2Info.java
+++ src/main/java/com/dace/dmgr/combat/character/vellion/action/VellionP2Info.java
@@ -1,7 +1,10 @@
 package com.dace.dmgr.combat.character.vellion.action;
 
+import com.dace.dmgr.combat.action.TextIcon;
 import com.dace.dmgr.combat.action.info.PassiveSkillInfo;
 import lombok.Getter;
+
+import java.text.MessageFormat;
 
 public final class VellionP2Info extends PassiveSkillInfo<VellionP2> {
     /** 치유 피해량 비율 */
@@ -10,6 +13,10 @@ public final class VellionP2Info extends PassiveSkillInfo<VellionP2> {
     private static final VellionP2Info instance = new VellionP2Info();
 
     private VellionP2Info() {
-        super(VellionP2.class, "마력 흡수");
+        super(VellionP2.class, "마력 흡수",
+                "",
+                "§f▍ 적에게 피해를 입히면 §a" + TextIcon.HEAL + " 회복§f합니다.",
+                "",
+                MessageFormat.format("§a{0}§f 피해량의 {1}%", TextIcon.HEAL, 100 * HEAL_DAMAGE_RATIO));
     }
 }
