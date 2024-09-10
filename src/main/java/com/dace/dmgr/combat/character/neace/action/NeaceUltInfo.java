934d8eeae87891b811c1490dac0f398072eb4b36
dkdace <darkdace2@naver.com>
2024-09-11 00:13:56 +0900

    '니스' - 동작 설명 개편

--- src/main/java/com/dace/dmgr/combat/character/neace/action/NeaceUltInfo.java
+++ src/main/java/com/dace/dmgr/combat/character/neace/action/NeaceUltInfo.java
@@ -1,7 +1,10 @@
 package com.dace.dmgr.combat.character.neace.action;
 
+import com.dace.dmgr.combat.action.TextIcon;
 import com.dace.dmgr.combat.action.info.UltimateSkillInfo;
 import lombok.Getter;
+
+import java.text.MessageFormat;
 
 public final class NeaceUltInfo extends UltimateSkillInfo<NeaceUlt> {
     /** 궁극기 필요 충전량 */
@@ -16,6 +19,15 @@ public final class NeaceUltInfo extends UltimateSkillInfo<NeaceUlt> {
     private static final NeaceUltInfo instance = new NeaceUltInfo();
 
     private NeaceUltInfo() {
-        super(NeaceUlt.class, "치유의 성역");
+        super(NeaceUlt.class, "치유의 성역",
+                "",
+                "§f▍ 체력을 최대치로 즉시 §a" + TextIcon.HEAL + " 회복§f하고 일정 시간동안",
+                "§f▍ 여러 대상을 자동으로 치유합니다.",
+                "§f▍ 사용 중에는 공격할 수 없습니다.",
+                "",
+                MessageFormat.format("§f{0} {1}", TextIcon.ULTIMATE, COST),
+                MessageFormat.format("§7{0}§f {1}초", TextIcon.DURATION, DURATION / 20.0),
+                "",
+                "§7§l[4] §f사용");
     }
 }
