62ce693133f2e5fb3d284f5711c2c4c5629c9c77
dkdace <darkdace06@gmail.com>
2024-10-07 11:15:27 +0900

    EventManager의 불필요한 주석 제거

--- src/main/java/com/dace/dmgr/event/EventManager.java
+++ src/main/java/com/dace/dmgr/event/EventManager.java
@@ -47,8 +47,6 @@ public final class EventManager {
         EventUtil.registerPacketListener(new OnPlayServerNamedSoundEffect());
         EventUtil.registerPacketListener(new OnPlayServerCustomSoundEffect());
         EventUtil.registerPacketListener(new OnPlayServerAbilities());
-//        EventUtil.registerPacketListener(new OnPlayClientLook());
-//        EventUtil.registerPacketListener(new OnPlayClientPositionLook());
         EventUtil.registerPacketListener(new OnPlayServerPlayerInfo());
 
         ConsoleLogger.info("이벤트 등록 완료");
