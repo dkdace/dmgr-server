d879e76a1650fa604a43527c37621ea524b0b2d1
dkdace <darkdace2@naver.com>
2024-10-12 21:32:37 +0900

    SelectChar(전투원 선택 GUI) - 팀원이 선택한 전투원 표시

--- src/main/java/com/dace/dmgr/item/gui/SelectChar.java
+++ src/main/java/com/dace/dmgr/item/gui/SelectChar.java
@@ -2,15 +2,22 @@ package com.dace.dmgr.item.gui;
 
 import com.dace.dmgr.combat.character.CharacterType;
 import com.dace.dmgr.combat.character.Role;
+import com.dace.dmgr.combat.entity.CombatUser;
+import com.dace.dmgr.game.GameUser;
 import com.dace.dmgr.item.ItemBuilder;
 import com.dace.dmgr.item.StaticItem;
+import com.dace.dmgr.user.User;
+import com.dace.dmgr.util.task.IntervalTask;
+import com.dace.dmgr.util.task.TaskUtil;
 import lombok.Getter;
 import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
 
 /**
  * 전투원 선택 GUI 클래스.
@@ -25,6 +32,8 @@ public final class SelectChar extends Gui {
 
     @Override
     public void onOpen(@NonNull Player player, @NonNull GuiController guiController) {
+        User user = User.fromPlayer(player);
+
         guiController.fillColumn(2, DisplayItem.EMPTY.getStaticItem());
         guiController.set(0, SelectCharInfoItem.SCUFFLER.staticItem);
         guiController.set(9, SelectCharInfoItem.MARKSMAN.staticItem);
@@ -33,13 +42,38 @@ public final class SelectChar extends Gui {
         guiController.set(36, SelectCharInfoItem.SUPPORT.staticItem);
         guiController.set(45, SelectCharInfoItem.CONTROLLER.staticItem);
 
-        int[] indexes = {2, 11, 20, 29, 38, 47};
+        TaskUtil.addTask(user, new IntervalTask(i -> {
+            if (!player.getOpenInventory().getTitle().equals("§c§l전투원 선택"))
+                return false;
 
-        for (CharacterType characterType : CharacterType.values()) {
-            int i = indexes[characterType.getCharacter().getRole().ordinal()]++;
+            CombatUser combatUser = CombatUser.fromUser(user);
+            if (combatUser == null)
+                return true;
 
-            guiController.set(i, CharacterType.valueOf(characterType.toString()).getGuiItem());
-        }
+            GameUser gameUser = combatUser.getGameUser();
+
+            if (i % 10 == 0) {
+                int[] indexes = {2, 11, 20, 29, 38, 47};
+
+                for (CharacterType characterType : CharacterType.values()) {
+                    int index = indexes[characterType.getCharacter().getRole().ordinal()]++;
+                    boolean isDuplicated = gameUser != null && gameUser.getTeam() != null &&
+                            Arrays.stream(gameUser.getTeam().getTeamUsers()).anyMatch(targetGameUser -> {
+                                CombatUser targetCombatUser = CombatUser.fromUser(targetGameUser.getUser());
+                                Validate.notNull(targetCombatUser);
+
+                                return targetCombatUser.getCharacterType() == characterType;
+                            });
+
+                    guiController.set(index, CharacterType.valueOf(characterType.toString()).getGuiItem(), itemBuilder -> {
+                        if (isDuplicated)
+                            itemBuilder.addLore("", "§c§l팀원이 이미 선택했습니다.");
+                    });
+                }
+            }
+
+            return true;
+        }, 1));
     }
 
     private enum SelectCharInfoItem {
