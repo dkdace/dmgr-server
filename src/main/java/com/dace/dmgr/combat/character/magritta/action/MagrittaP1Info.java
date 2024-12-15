ac8fc044eeb5c4dd163251e62581799c77503fe7
dkdace <darkdace2@naver.com>
2024-12-15 22:50:58 +0900

    '마그리타' 패시브 1번 - 자신에게 발동 불가능, 회복량 및 지속시간 증가

--- src/main/java/com/dace/dmgr/combat/character/magritta/action/MagrittaP1Info.java
+++ src/main/java/com/dace/dmgr/combat/character/magritta/action/MagrittaP1Info.java
@@ -8,11 +8,11 @@ import lombok.Getter;
 
 public final class MagrittaP1Info extends PassiveSkillInfo<MagrittaP1> {
     /** 초당 치유량 */
-    public static final int HEAL_PER_SECOND = 80;
+    public static final int HEAL_PER_SECOND = 100;
     /** 감지 범위 (단위: 블록) */
     public static final double DETECT_RADIUS = 15;
     /** 지속시간 (tick) */
-    public static final long DURATION = (long) (1.5 * 20);
+    public static final long DURATION = 2 * 20L;
     @Getter
     private static final MagrittaP1Info instance = new MagrittaP1Info();
 
