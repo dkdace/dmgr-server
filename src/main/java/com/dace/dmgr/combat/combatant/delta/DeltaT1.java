b11e65e3a68e3d3f707f4f2739a0429b32fd7542
Bonwoong <thesmartist@naver.com>
2025-05-01 05:38:32 +0900

    Refactor: DeltaT1Info DynamicTraitInfo

--- src/main/java/com/dace/dmgr/combat/combatant/delta/DeltaT1.java
+++ src/main/java/com/dace/dmgr/combat/combatant/delta/DeltaT1.java
@@ -1,4 +1,12 @@
 package com.dace.dmgr.combat.combatant.delta;
 
-public final class DeltaT1 {
+import com.dace.dmgr.combat.action.Trait;
+import com.dace.dmgr.combat.action.info.DynamicTraitInfo;
+import com.dace.dmgr.combat.entity.combatuser.CombatUser;
+import lombok.NonNull;
+
+public final class DeltaT1 extends Trait {
+    public DeltaT1(@NonNull CombatUser combatUser, @NonNull DynamicTraitInfo<?> traitInfo) {
+        super(combatUser, traitInfo);
+    }
 }
--- src/main/java/com/dace/dmgr/combat/combatant/delta/DeltaT1Info.java
+++ src/main/java/com/dace/dmgr/combat/combatant/delta/DeltaT1Info.java
@@ -2,11 +2,11 @@ package com.dace.dmgr.combat.combatant.delta;
 
 import com.dace.dmgr.combat.action.TextIcon;
 import com.dace.dmgr.combat.action.info.ActionInfoLore;
-import com.dace.dmgr.combat.action.info.TraitInfo;
+import com.dace.dmgr.combat.action.info.DynamicTraitInfo;
 import lombok.Getter;
 import org.bukkit.ChatColor;
 
-public class DeltaT1Info extends TraitInfo {
+public class DeltaT1Info extends DynamicTraitInfo<DeltaT1> {
     /** 최대치 */
     public static final int MAX = 128;
 
@@ -14,11 +14,11 @@ public class DeltaT1Info extends TraitInfo {
     private static final DeltaT1Info instance = new DeltaT1Info();
 
     private DeltaT1Info() {
-        super("글리치",
+        super(DeltaT1.class, "글리치",
                 new ActionInfoLore(ActionInfoLore.Section
                         .builder("수치에 비례하여 기본 무기의 공격력이 증가하는 고유 자원입니다. 피해를 받으면 감소합니다.")
-                        .addValueInfo(TextIcon.COOLDOWN, "최대 " + MAX, ChatColor.GREEN)
-                        .addValueInfo(TextIcon.COOLDOWN_DECREASE, "(받은 피해) × 0.15", ChatColor.GREEN)
+                        .addValueInfo(TextIcon.GLITCH, "최대 " + MAX)
+                        .addValueInfo(TextIcon.GLITCH_DECREASE, "(받은 피해) × 0.15", ChatColor.RED)
                         .build()));
     }
 }
