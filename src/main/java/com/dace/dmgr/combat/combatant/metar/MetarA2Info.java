8a9dd1403be75e0770c814702c30e06fb47554eb
dkdace <darkdace2@naver.com>
2025-05-20 23:18:20 +0900

    'METAR' 액티브 2번 - 쿨타임 증가 및 지속시간 감소

--- src/main/java/com/dace/dmgr/combat/combatant/metar/MetarA2Info.java
+++ src/main/java/com/dace/dmgr/combat/combatant/metar/MetarA2Info.java
@@ -17,13 +17,13 @@ public final class MetarA2Info extends ActiveSkillInfo<MetarA2> {
     /** 쿨타임 */
     public static final Timespan COOLDOWN = Timespan.ofSeconds(0.5);
     /** 스택 충전 쿨타임 */
-    public static final Timespan STACK_COOLDOWN = Timespan.ofSeconds(7);
+    public static final Timespan STACK_COOLDOWN = Timespan.ofSeconds(8);
     /** 최대 스택 충전량 */
     public static final int MAX_STACK = 2;
     /** 체력 */
     public static final int HEALTH = 1000;
     /** 지속시간 */
-    public static final Timespan DURATION = Timespan.ofSeconds(14);
+    public static final Timespan DURATION = Timespan.ofSeconds(10);
 
     /** 방어 점수 */
     public static final int BLOCK_SCORE = 20;
